b540bf77c2b9fc34fcb97f9b1ca274f6f8a67f85
Fadi Zahhar <Fadi Zahhar>
2016-09-15 15:45:35 +0300

    Creating the home page and doing some fixes

--- js/main.js
+++ js/main.js
@@ -1,7 +1,26 @@
+function reset() {
+  $('.lorem').attr('src','images/lorem.png').attr('style','padding-left:0px;padding-top:25px');
+  $('.lorem2').remove();  
+}
 $(document).ready(function() {
    $('button.contact').on('click',function(e){
     $( ".contactus" ).slideToggle( "slow", function() {
     // Animation complete.
     });
+   });
+
+   $('img.find').on('click',function(e){
+    $('img.find').removeClass('find').addClass('search').attr('src','images/search.png');
+    $('.fa-close').show('slow');
+   });
+
+   $('.fa-close').on('click',function(e){
+    $('img.search').removeClass('search').addClass('find').attr('src','images/find.png');
+    $('.fa-close').hide('slow');
+   });
+
+   $('.lorem').on('click',function(e){
+    $('.lorem2').remove();  
+    $('.lorem').attr('src','images/loremempty.png').attr('style','padding-left:25px;padding-top:25px').after('<img class="lorem2" src="images/lorem-.png" style="padding-left:25px;cursor:pointer" onclick="reset()" />');
    });
 });
